85efc0740663eb59a1b8a50563adb997e1a16ac3
dhruv <dhruvprajapati116@gmail.com>
2022-03-28 15:37:27 +0530

    Working Clock with python and tkinter

--- clock.py
+++ clock.py
@@ -3,6 +3,15 @@ from time import strftime
 
 screen = Tk()
 screen.title("Digital Clock")
+screen.geometry("550x100")
 screen.configure(bg="black")
 
+def time():
+    lbl.config(text = strftime("%H:%M:%S"))
+    lbl.after(1000,time)
+
+lbl = Label(screen,bg="black",fg="green",font="arial 50 italic")
+lbl.pack(anchor="center", fill="both",expand = 1)
+
+time()
 mainloop()
